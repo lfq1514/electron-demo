9530345cf00d8cf5b670ae44f353b40545165987
刘飞强 <liufqiang@yonyou.com>
2023-03-24 17:42:54 +0800

    feat: 添加主进程与渲染进程通讯代码逻辑

--- main.js
+++ main.js
@@ -1,17 +1,53 @@
-const { app, BrowserWindow } = require("electron");
-const path = require('path')
+const { app, BrowserWindow,ipcMain } = require("electron");
+const path = require("path");
+const http=require('http')
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    frame:true,//设置为无边框窗口，默认为true
+    width: 800,//窗口宽度
+    height: 600,//窗口高度
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
+      preload: path.join(__dirname, "preload.js"),
+    },
   });
 
+
   win.loadFile("index.html");
+  ipcMain.handle('get-printlist',async ()=>{
+    return await win.webContents.getPrintersAsync()
+  })
+
+  ipcMain.handle('handle-print',async (e,defalutPrint)=>{
+    console.log('defalutPrint',defalutPrint)
+    const win = new BrowserWindow({
+     show:false
+    });
+    // let pdfurl='https://cwydzkj-idoc-rds-test.oss-cn-beijing.aliyuncs.com/test/2023/04/3091ee2f-2b8a-425d-92d3-03adebceebf9.pdf?Expires=1679628797&OSSAccessKeyId=LTAI5tJ2heTN5rxXxjgr92N3&Signature=GHnNnoERzS%2F8R9ZnSjRkANn5ssg%3D&response-content-type=application%2Foctet-stream'
+    win.loadURL()
+    win.webContents.on('did-finish-load',()=>{
+  
+    })
+
+
+
+   
+    win.webContents.print({
+      silent:false,
+    })
+  })
+  
 };
 app.whenReady().then(() => {
   createWindow();
 });
+app.on('login', (event, webContents, details, authInfo, callback) => {
+  // event.preventDefault()
+  callback('username', 'secret')
+})
+//最后一个窗口关闭时，退出应用
+//如果你没有监听此事件并且所有窗口都关闭了，默认的行为是控制退出程序
+app.on('window-all-closed',()=>{
+  app.quit()
+})
+
--- renderer.js
+++ renderer.js
@@ -1,1 +1,14 @@
-console.log('i am renderer')
+const btn1=document.getElementsByClassName('btn1')[0]
+const btn2=document.getElementsByClassName('btn2')[0]
+const printListBox=document.getElementsByClassName('showList')[0]
+let _printPrint={}
+btn1.addEventListener('click',async function(){
+   const printList= (await window.electronAPI.getPrintList()).map(item=>({name:item.name,isDefault:item.isDefault}))
+   _printPrint=printList.find(item=>item.isDefault)
+   printListBox.innerHTML=JSON.stringify(printList)
+})
+btn2.addEventListener('click',function(){
+    console.log('render---handlePrint')
+    window.electronAPI.handlePrint(_printPrint)
+
+})
